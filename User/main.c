4ae5bee4418ed20a92700e19b38e518bba5b7f6c
kaidegit <2857693944@qq.com>
2022-01-21 20:47:00 +0800

    test openocd

--- User/main.c
+++ User/main.c
@@ -21,7 +21,20 @@
 
 /* Global define */
 
+#define LED_GPIO_Port GPIOC
+#define LED_Pin GPIO_Pin_13
+
 /* Global Variable */
+
+void LED_GPIO_Init(void) {
+    GPIO_InitTypeDef GPIO_InitStructure;
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+    GPIO_InitStructure.GPIO_Pin = LED_Pin;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(LED_GPIO_Port, &GPIO_InitStructure);
+}
 
 
 /*******************************************************************************
@@ -30,18 +43,21 @@
 * Input          : None
 * Return         : None
 *******************************************************************************/
-int main(void)
-{
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
-	Delay_Init();
-	USART_Printf_Init(115200);
-	printf("SystemClk:%d\r\n",SystemCoreClock);
+int main(void) {
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+    Delay_Init();
+    USART_Printf_Init(115200);
+    printf("SystemClk:%d\r\n", SystemCoreClock);
 
-	printf("This is printf example\r\n");
+    LED_GPIO_Init();
 
-	while(1)
-	{
+    printf("This is printf example111\r\n");
 
-	}
+    while (1) {
+        GPIO_WriteBit(LED_GPIO_Port, LED_Pin, 1);
+        Delay_Ms(250);
+        GPIO_WriteBit(LED_GPIO_Port, LED_Pin, 0);
+        Delay_Ms(250);
+    }
 }
 
