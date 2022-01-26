7b29b5042d94487bfa28d82174cf21a8df34cd8a
kaidegit <2857693944@qq.com>
2022-01-26 22:31:44 +0800

    add usart2 irq test

--- User/main.c
+++ User/main.c
@@ -6,14 +6,6 @@
 * Description        : Main program body.
 *******************************************************************************/
 
-/*
- *@Note
- 串口打印调试例程：
- USART1_Tx(PA9)。
- 本例程演示使用 USART1(PA9) 作打印调试口输出。
-
-*/
-
 #include "debug.h"
 
 
@@ -26,6 +18,8 @@
 
 /* Global Variable */
 
+
+
 void LED_GPIO_Init(void) {
     GPIO_InitTypeDef GPIO_InitStructure;
 
@@ -36,28 +30,65 @@ void LED_GPIO_Init(void) {
     GPIO_Init(LED_GPIO_Port, &GPIO_InitStructure);
 }
 
+void USART2_Init() {
+    GPIO_InitTypeDef GPIO_InitStructure;
+    USART_InitTypeDef USART_InitStructure;
+    NVIC_InitTypeDef NVIC_InitStructure;
 
-/*******************************************************************************
-* Function Name  : main
-* Description    : Main program.
-* Input          : None
-* Return         : None
-*******************************************************************************/
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+    /* USART2 TX-->A.2   RX-->A.3 */
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    USART_InitStructure.USART_BaudRate = 115200;
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART_InitStructure.USART_StopBits = USART_StopBits_1;
+    USART_InitStructure.USART_Parity = USART_Parity_No;
+    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+
+    USART_Init(USART2, &USART_InitStructure);
+    USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
+
+    NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+
+    USART_Cmd(USART2, ENABLE);
+}
+
+__attribute__((interrupt("WCH-Interrupt-fast"))) void USART2_IRQHandler(void) {
+    if (USART_GetITStatus(USART2, USART_IT_RXNE) != RESET) {
+        while (USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET);
+        USART_SendData(USART1, USART_ReceiveData(USART2));
+    }
+}
+
 int main(void) {
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
     Delay_Init();
     USART_Printf_Init(115200);
+    USART2_Init();
     printf("SystemClk:%d\r\n", SystemCoreClock);
 
     LED_GPIO_Init();
 
-    printf("This is printf example111\r\n");
+    printf("This is a clion template for ch32v103\r\n");
 
     while (1) {
         GPIO_WriteBit(LED_GPIO_Port, LED_Pin, 1);
         Delay_Ms(1000);
         GPIO_WriteBit(LED_GPIO_Port, LED_Pin, 0);
-        Delay_Ms(250);
+        Delay_Ms(1000);
     }
 }
 
